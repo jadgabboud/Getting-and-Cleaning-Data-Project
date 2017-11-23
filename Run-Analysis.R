b18911db1d6fec1988313ce09a69bd914e081d1b
jad abboud <jadgabboud@gmail.com>
2017-11-24 00:21:40 +0200

    Adjusted

--- Run-Analysis.R
+++ Run-Analysis.R
@@ -1,14 +1,3 @@
-library(plyr)
-
-# 0. Downloading dataset
-if(!file.exists("./data")){dir.create("./data")}
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileUrl,destfile="./data/Dataset.zip")
-
-# Unzip dataSet to /data directory
-unzip(zipfile="./data/Dataset.zip",exdir=""/Users/JGA"")
-
-
 # 1. Merging the training and the test sets to create one data set:
 
 # 1.1 Reading files
